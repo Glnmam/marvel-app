616397c688528bf4925ac2cf5525a6b91ec2f9cb
Glenn <glenn.madzou.a.miere@etu.univ-poitiers.fr>
2023-11-13 17:42:52 +0100

    Fix unit test for App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import './style.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-
-  const h1Element = screen.getByRole('heading', { level: 1, name: "Marvel App" });
-  expect(h1Element).toBeInTheDocument();
-});
 
 import routes from './routes';
 
